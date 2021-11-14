8e0c3de6f27390b0e3a35aba1cb4c366846994d7
AgustinDiazArnaudo <adiazarnaudo@gmail.com>
2021-11-14 19:42:30 -0300

    mercadoLiebre completo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const path = require("path");
 const app = express();
 
 app.use(express.static('public'));
@@ -9,5 +9,13 @@ app.listen(process.env.PORT || 3000, ()=>{
 });
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(path.join(__dirname + '/views/login.html'));
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(path.join(__dirname + '/views/register.html'));
 });
